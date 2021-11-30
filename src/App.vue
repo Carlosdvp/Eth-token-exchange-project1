8af1af667e5f322d9e1714fb30a3cf4a3444ebe2
Carlos V <carlosdvp.tx@gmail.com>
2021-11-30 19:15:04 -0400

    made initial commit of te base files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Dapp #1" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
